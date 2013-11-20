9910d33a8555ce10db84c2796e76540913179717
maximus5684 <maximus5684@gmail.com>
2013-11-19 18:26:31 -0600

    Added basic files for Character Builder page 2.

---
+++ character_builder2.php
@@ -0,0 +1,16 @@
+<?php 
+
+$pageTitle = 'Character Builder - Iron Kingdoms Character Generator'; 
+include($_SERVER['DOCUMENT_ROOT'] . '/phpincludes/header1.php'); ?> 
+  
+        <link href="css/character_builder2.css" rel="stylesheet"> 
+        <script src="js/races.js"></script> 
+        <script src="js/archetypes.js"></script> 
+        <script src="js/careers.js"></script> 
+        <script src="js/cb2.js"></script> 
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/phpincludes/header2.php'); ?>
+
+        <div class="container" id="mainContain" ng-controller="CB2Ctrl">
+
+        </div>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/phpincludes/footer.php'); ?>
---
+++ js/cb2.js
@@ -0,0 +1,2 @@
+function CB2Ctrl($scope) {
+}
